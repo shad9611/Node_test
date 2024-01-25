c28f9046ff08d33a89db99ba3ab5bca107d86683
Shadai Barraza <elohim_shadai@hotmail.com>
2024-01-24 23:31:50 -0700

    update

--- challenges/01.js
+++ challenges/01.js
@@ -3,12 +3,18 @@
 
 const { read, readNumber, print } = require('../helpers')
 
+const CalHypotenuse = (n1 , n2)=>{
+  const aPow = Math.pow(n1, 2)
+  const bPow = Math.pow(n2, 2)
+  const hypotenuse = Math.sqrt(aPow + bPow)
+  return hypotenuse
+}
+
 const main = async () => {
-    const n1 = await readNumber('Ingrese el valor del cateto A: ')
-    const n2 = await readNumber('Ingrese el valor del cateto B: ')
-    const aPow = Math.pow(n1, 2)
-    const bPow = Math.pow(n2, 2)
-    const hypotenuse = Math.sqrt(aPow + bPow)
-      print(`El valor de la hipotenusa es: ${hypotenuse}`)
+  const n1 = await readNumber('Ingrese el valor del cateto A: ')
+  const n2 = await readNumber('Ingrese el valor del cateto B: ')
+
+  const result = CalHypotenuse(n1 , n2)
+  print(`El valor de la hipotenusa es: ${result}`)
 }
 module.exports = main
