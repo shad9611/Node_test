75a22076fe95f3fcdfdc7ea82b050d5b2ef12f83
Shadai Barraza <elohim_shadai@hotmail.com>
2024-01-27 23:30:33 -0700

    Update 22.js

--- challenges/22.js
+++ challenges/22.js
@@ -6,8 +6,25 @@
 
 const { read, readNumber, print } = require('../helpers')
 
+const convertRgbToHex = rgb => {
+  const bra = rgb.indexOf("(" ) + 1
+  const ket = rgb.indexOf(")")
+  const str = rgb.substring(bra, ket)
+  const numberColor = str.split(',').map((str) => parseFloat(str.trim()))
+  if(numberColor.length === 3){
+    const hex = numberColor.map((value) => {
+      const valueHex = value.toString(16)
+      return valueHex.length === 1 ?  "0" + valueHex : valueHex
+    })
+    return hex.join("")
+  }else
+    return ""
+}
+
 const main = async () => {
-  print('Problema aún no resuelto')
+  const rgb = await read("Ingresa un color en Rgb")
+  const color = convertRgbToHex(rgb)
+  print(`El color Rgb para ${rgb} es: #${color.toUpperCase()}`)
 }
 
 module.exports = main
