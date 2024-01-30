f16e437af7e3f2a81d5bbd2c3c1c84032858d2e8
Shadai Barraza <elohim_shadai@hotmail.com>
2024-01-30 13:14:14 -0700

    Update 34.js

--- challenges/34.js
+++ challenges/34.js
@@ -2,8 +2,31 @@
 
 const { read, readNumber, print } = require('../helpers')
 
+const isPrime = num => {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0)
+      return false
+  }
+  return num > 1
+}
+
+const FirstNPrimes = (n) => {
+  const primes = []
+  let currentNumber = 2
+
+  while (primes.length < n) {
+    if (isPrime(currentNumber))
+      primes.push(currentNumber)
+    currentNumber++
+  }
+
+  return primes
+}
+
 const main = async () => {
-  print('Problema aún no resuelto')
+  const first100Primes = FirstNPrimes(100)
+  print("Los primeros 100 números primos son:")
+  print(first100Primes.join(", "))
 }
 
 module.exports = main
